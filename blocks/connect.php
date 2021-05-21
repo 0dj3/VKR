4743ea9c3bd0b2fab5a075630d286d8b4e002d2d
0dj3 <xaryshal@gmail.com>
2021-05-21 10:25:19 +0900

    re

--- blocks/connect.php
+++ blocks/connect.php
@@ -4,7 +4,7 @@
     $db_pass = 'root';
     $db_name = 'database';
     $db_host = 'localhost';
-    $db_port = 3307;
+    $db_port = 3306;
     //========= Подключения к БД =========//
     $mysql = @mysqli_connect("$db_host:$db_port",$db_user,$db_pass,$db_name);
 ?>
